da9e96f4c625fc94b71389e164dd343a15cfc32f
Edward Marinescu <edi.ultras@gmail.com>
2022-07-02 22:31:29 +0300

    Add files via upload

---
+++ config/config.go
@@ -0,0 +1,13 @@
+package config
+
+const (
+	//restApi config
+	BaseUrl       = "https://capi.bitget.com"
+	ApiKey        = ""
+	SecretKey     = ""
+	PASSPHRASE    = ""
+	TimeoutSecond = 30
+
+	//websocket config
+	WsUrl = "wss://ws.bitgetapi.com/spot/v1/stream"
+)
